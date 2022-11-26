cbb3967dc51c1e08339bf4250010937aa3f5a910
EnTIv <38716516+EnTIv@users.noreply.github.com>
2022-11-26 20:04:31 +0800

    修复拿着方块的末影人捕捉

--- src/main/kotlin/com/entiv/pokeballcatch/data/EndermanData.kt
+++ src/main/kotlin/com/entiv/pokeballcatch/data/EndermanData.kt
@@ -2,17 +2,25 @@ package com.entiv.pokeballcatch.data
 
 import de.tr7zw.nbtapi.NBTCompound
 import net.kyori.adventure.text.Component
-import org.bukkit.block.data.BlockData
+import org.bukkit.Material
 import org.bukkit.entity.Enderman
 
 
 object EndermanData : DataWrapper<Enderman>(Enderman::class) {
     override fun entityWriteToNbt(entity: Enderman, compound: NBTCompound) {
-        compound.setObject("carriedBlock", entity.carriedBlock)
+
+        compound.setString("carriedBlock", entity.carriedBlock?.material.toString())
+
+        entity.carriedBlock?.material?.let {
+            compound.setString("carriedBlock", it.toString())
+        }
     }
 
     override fun nbtWriteToEntity(compound: NBTCompound, entity: Enderman) {
-        entity.carriedBlock = compound.getObject("carriedBlock", BlockData::class.java)
+        val name = compound.getString("carriedBlock") ?: return
+        val blockData = Material.valueOf(name).createBlockData()
+
+        entity.carriedBlock = blockData
     }
 
     override fun entityWriteToComponent(entity: Enderman, components: MutableList<Component>) {
